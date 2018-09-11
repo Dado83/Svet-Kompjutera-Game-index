e0bd827f41782e2a5637192653a69d77f0947853
Dado83 <83deadpool@gmail.com>
2018-09-11 20:28:27 +0200

    fix tests

--- Test/Test.java
+++ Test/Test.java
@@ -1,32 +1,34 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.Set;
 import org.junit.Before;
 import SK.GameReview;
 import SK.Model;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 class Test {
 
-	GameReview igra = new GameReview("naslov", "autor", 90, "2018", "www.link.com");
-	Model model = new Model();
-	Set<GameReview> modelSet;
-	
-	@Before
-	private void init() {
-		modelSet = model.getGameIndex();
-	}
-	
-	@org.junit.jupiter.api.Test
-	void testIgra() {
-		assertEquals("naslov", igra.getTitle());
-		assertEquals("autor", igra.getAuthor());
-		assertEquals(90, igra.getScore());
-		assertEquals("2018", igra.getYear());
-	}
-	
-	@org.junit.jupiter.api.Test
-	void testModel() {
-		assertNotNull(model.getGameIndex());
-	}
+    GameReview igra = new GameReview("naslov", "autor", 90, "2018", "www.link.com");
+    Model model = new Model();
+    Set<GameReview> modelSet;
+
+    @Before
+    private void init() {
+        modelSet = model.getGameIndex();
+    }
+
+    @org.junit.Test
+    void testIgra() {
+        assertEquals("naslov", igra.getTitle());
+        assertEquals("autor", igra.getAuthor());
+        assertEquals(90, igra.getScore());
+        assertEquals("2018", igra.getYear());
+    }
+
+    @org.junit.Test
+    void testModel() {
+        assertNotNull(model.getGameIndex());
+    }
 
 }
